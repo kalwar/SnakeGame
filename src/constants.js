2f1267bcce9ea846c251973baa070f19baadcaf3
Santosh <santoshkalwar@gmail.com>
2023-12-10 14:09:07 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,7 @@ const APPLE_START = [8, 3];
 /* SCALE is a number that represents the size of each square on the grid. The scale is 40 pixels, which means that each square is 40 by 40 pixels. */
 const SCALE = 40;
 
-/* SPEED is a number that represents the speed of the snake's movement. The speed is 100 milliseconds, which means that the snake moves one square every 0.1 seconds. */
+/* SPEED is a number that represents the speed of the snake's movement. The speed is 500 milliseconds, which means that the snake moves one square every 0.5 seconds. */
 const SPEED = 500;
 
 /* DIRECTIONS is an object that maps the key codes of the arrow keys to the corresponding direction vectors. The direction vector is an array that contains the horizontal and vertical components of the movement. For example, the up arrow key has the code 38 and the direction vector [0, -1], which means that pressing the up arrow key will move the snake one square up. */
